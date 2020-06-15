55b3d12d8c31b03b2bb1b8d262e96d1d24fe38e9
ili2908 <illia.kovalenko@ukma.edu.ua>
2020-06-15 18:08:54 +0300

    'm'

--- Renderer.py
+++ Renderer.py
@@ -1,4 +1,25 @@
 class Renderer:
-    def __init__():
-        pass
+    def __init__(self,gui):
+        self.tileWidth=gui.size
+        self.gui=gui
+        self.x=self.gui.x
+        self.y=self.gui.y
+    def drawChunk(self,canvas,chunk):
+        
+    def drawTile(self,tile,size):
+        x= self.x+self.tileWidth*(tile.x+size*cx
+        y= tile.y*10+180*cy
+        if not tile.connections[0]:
+            self.canvas.create_line(x,y,x+10,y)
+        if not tile.connections[1]:
+            self.canvas.create_line(x,y,x,y+10)
+        if not tile.connections[2]:
+            self.canvas.create_line(x+10,y,x+10,y+10)
+        if not tile.connections[3]:
+            self.canvas.create_line(x,y+10,x+10,y+10)
+    
+    def drawWalls(x1,y1,x2,y2):
+        
+        
+        
     
--- test.py
+++ test.py
@@ -4,20 +4,20 @@ class Example(tk.Frame):
     def __init__(self, root):
         tk.Frame.__init__(self, root)
         self.canvas = tk.Canvas(self, width=400, height=400, background="bisque")
-        self.xsb = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
-        self.ysb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
-        self.canvas.configure(yscrollcommand=self.ysb.set, xscrollcommand=self.xsb.set)
-        self.canvas.configure(scrollregion=(0,0,1000,1000))
+        #self.xsb = tk.Scrollbar(self, orient="horizontal", command=self.canvas.xview)
+        #self.ysb = tk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+        #self.canvas.configure(yscrollcommand=self.ysb.set, xscrollcommand=self.xsb.set)
+        self.canvas.configure(scrollregion=(0,0,10000,10000))
 
-        self.xsb.grid(row=1, column=0, sticky="ew")
-        self.ysb.grid(row=0, column=1, sticky="ns")
+        #self.xsb.grid(row=1, column=0, sticky="ew")
+        #self.ysb.grid(row=0, column=1, sticky="ns")
         self.canvas.grid(row=0, column=0, sticky="nsew")
         self.grid_rowconfigure(0, weight=1)
         self.grid_columnconfigure(0, weight=1)
 
-        for n in range(50):
-            x0 = random.randint(0, 900)
-            y0 = random.randint(50, 900)
+        for n in range(100000):
+            x0 = random.randint(0, 10000)
+            y0 = random.randint(50, 10000)
             x1 = x0 + random.randint(50, 100)
             y1 = y0 + random.randint(50,100)
             color = ("red", "orange", "yellow", "green", "blue")[random.randint(0,4)]
