78f7b45bd46c91e23804c98c32a8d7a0fc80d0f8
Andrii Chaliuk <0632135592an@gmail.com>
2020-06-15 18:14:11 +0300

    Add start_window

--- food.py
+++ food.py
@@ -1,6 +1,24 @@
 from random import *
-
-allFoods = {"name1": ["skin", "arr_nutritional_value", "rotten_skin", "coefficient_of_rotten", "rottenness (є продукти, які взагалі не псуються)"]}
+from tkinter import *
+allFoods = {"apple": [PhotoImage(file='img/food/Food_apple.png'), 10, 0.05, True],
+            "apple2": [PhotoImage(file="img/food/Food_apple2.png"), 10, 0.05, True],
+            "bear": [PhotoImage(file="img/food/Food_bear.png"), 10, 0.055, True],
+            "borshch": [PhotoImage(file="img/food/Food_borshch.png"), 50, 0.035, True],
+            "cheese": [PhotoImage(file="img/food/Food_cake.png"), 25, 0.04, True],
+            "chocolate": [PhotoImage(file="img/food/Food_chocolate.png"), 25, 0.045, True],
+            "close_pan": [PhotoImage(file="img/food/Food_close_pan.png"), 25, 0.01, True],
+            "egg": [PhotoImage(file="img/food/Food_egg.png"), 15, 0.04, True],
+            "empty_pan": [PhotoImage(file="img/food/Food_empty_pan.png"), 0, 0, False],
+            "fish": [PhotoImage(file="img/food/Food_fish.png"), 20, 0.035, True],
+            "honey": [PhotoImage(file="img/food/Food_honey.png"), 25, 0, False],
+            "kiwi": [PhotoImage(file="img/food/Food_kiwi.png"), 15, 0.03, True],
+            "lemon": [PhotoImage(file="img/food/Food_lemon.png"), 10, 0.02, True],
+            "mushroom": [PhotoImage(file="img/food/Food_mushroom.png"), 20, 0.04, False],
+            "orange": [PhotoImage(file="img/food/Food_orange.png"), 10, 0.04, True],
+            "pizza": [PhotoImage(file="img/food/Food_pizza.png"), 20, 0.025, True],
+            "varenics": [PhotoImage(file="img/food/Food_varenics.png"), 50, 0.04, True],
+            "watermelon": [PhotoImage(file="img/food/Food_watermelon.png"), 15, 0.035, True],
+            "meat": [PhotoImage(file="img/food/meat.png"), 20, 0.04, True]}
 
 class Food():
     def __init__(self, name):
@@ -10,9 +28,9 @@ class Food():
         self.coords = {}
         self.coords.x = 0
         self.coords.y = 0
-        self.rotten_skin = allFoods[name][2]
-        self.rottenness = allFoods[name][4]
-        self.coefficient = allFoods[name][3]
+        self.rotten = 0
+        self.rottenness = allFoods[name][3]
+        self.coefficient = allFoods[name][2]
         self.isEaten = False
         self.value = randint(0.7 * allFoods[self.name][1], 1.3 * allFoods[self.name][1])
 
@@ -21,8 +39,8 @@ class Food():
         self.isEaten = True
 
     def rottenness(self):
-        if self.rottenness + self.value * self.coefficient >= 100:
+        if self.rottenness and self.rotten + self.value * self.coefficient >= 100:
             self.eaten()
             return
-        else:
-            self.rottenness += self.value * self.coefficient
+        elif self.rottenness and self.rotten + self.value * self.coefficient < 100:
+            self.rotten += self.value * self.coefficient
---
+++ start_window.py
@@ -0,0 +1,287 @@
+from tkinter import *
+from PIL import Image, ImageTk
+from functools import partial
+from tkinter.ttk import *
+class Start_window():
+    def __init__(self):
+        self.window = Tk()
+        self.window.title("Game")
+        width = self.window.winfo_screenwidth()
+        height = self.window.winfo_screenheight()
+        width = width // 2
+        height = height // 2
+        width = width - 250
+        height = height - 250
+        self.window.geometry('500x500+{}+{}'.format(width, height))
+        self.window.resizable(False, False)
+        self.canvas = Canvas(self.window, height=500, width=500)
+        self.canvas.pack()
+
+        img = PhotoImage(file='img/start_window_last.png')
+        img2 = PhotoImage(file='img/start.png')
+
+        self.bg = self.canvas.create_image(0, 0, image=img, anchor=NW)
+        self.start = self.canvas.create_image(75, 370/2, image=img2, anchor=NW)
+        self.motion()
+        self.canvas.tag_bind(self.start, '<Button-1>', self.main_window)
+        self.window.mainloop()
+
+    def motion(self):
+        self.canvas.move(self.start, 0, -1)
+        if self.canvas.coords(self.start)[1] > 370/2-30:
+            self.window.after(15, self.motion)
+
+    def main_window(self, event):
+        if self.bg:
+            self.canvas.delete(self.bg)
+        if self.start:
+            self.canvas.delete(self.start)
+        self.menu_icon = Image.open("img/menu_button.png")
+        self.menu = self.menu_icon.resize((50, 50), Image.ANTIALIAS)
+        self.menu = ImageTk.PhotoImage(self.menu)
+        self.menu_canv = self.canvas.create_image(0, 0, image=self.menu, anchor=NW)
+        self.canvas.tag_bind(self.menu_canv, '<Button-1>', self.pers_choice)
+        self.selected_pers = Image.open("img/Pers1.png")
+        self.selected_pers = self.selected_pers.resize((350, 350), Image.ANTIALIAS)
+        self.selected_pers = ImageTk.PhotoImage(self.selected_pers)
+        self.canvas.create_image(250, 150, image=self.selected_pers)
+
+    def pers_choice(self, event):
+        self.canvas.delete(self.menu)
+        self.menu_icon = self.menu_icon.rotate(90)
+        self.menu_rotated = self.menu_icon.resize((50, 50), Image.ANTIALIAS)
+        self.menu_rotated = ImageTk.PhotoImage(self.menu_rotated)
+        self.menu = self.canvas.create_image(0, 0, image=self.menu_rotated, anchor=NW)
+        self.pers_choice_label = Label(self.canvas, width=64)
+        self.pers_choice_label.place(x=50, y=0)
+        self.frame_pers1 = Frame(self.pers_choice_label, width=225, height=250)
+        self.frame_pers1.grid(column=0, row=0)
+
+        self.frame_pers2 = Frame(self.pers_choice_label, width=225, height=250)
+        self.frame_pers2.grid(column=1, row=0)
+
+        self.frame_pers3 = Frame(self.pers_choice_label, width=225, height=250)
+        self.frame_pers3.grid(column=0, row=1)
+
+        self.frame_pers4 = Frame(self.pers_choice_label, width=225, height=250)
+        self.frame_pers4.grid(column=1, row=1)
+
+        self.pers1_canv = Canvas(self.frame_pers1, width=222, height=247, bg="red")
+        self.pers1_canv.pack()
+
+        self.pers2_canv = Canvas(self.frame_pers2, width=222, height=247, bg="blue")
+        self.pers2_canv.pack()
+
+        self.pers3_canv = Canvas(self.frame_pers3, width=222, height=247, bg="yellow")
+        self.pers3_canv.pack()
+
+        self.pers4_canv = Canvas(self.frame_pers4, width=222, height=247, bg="green")
+        self.pers4_canv.pack()
+        self.selected_pers = None
+
+        pers1_icon = Image.open("img/Pers1.png")
+        self.pers1_canv.icon = pers1_icon
+        pers1_icon = pers1_icon.resize((222, 222), Image.ANTIALIAS)
+        pers1_icon = ImageTk.PhotoImage(pers1_icon)
+        self.pers1 = self.pers1_canv.create_image(0, 15, image=pers1_icon, anchor=NW)
+        self.pers1_canv.image = pers1_icon
+        self.pers1_canv.bind("<Button-1>", partial(self.click_color_config, self.pers1_canv, "darkred"))
+        self.pers1_canv.tag_bind(self.pers1, "<Button-3>", self.properties_pers1)
+
+        pers2_icon = Image.open("img/Pers4.png")
+        self.pers2_canv.icon = pers2_icon
+        pers2_icon = pers2_icon.resize((222, 222), Image.ANTIALIAS)
+        pers2_icon = ImageTk.PhotoImage(pers2_icon)
+        self.pers2 = self.pers2_canv.create_image(0, 15, image=pers2_icon, anchor=NW)
+        self.pers2_canv.image = pers2_icon
+        self.pers2_canv.bind("<Button-1>", partial(self.click_color_config, self.pers2_canv, "lightblue"))
+        self.pers2_canv.tag_bind(self.pers2, "<Button-3>", self.properties_pers2)
+
+        pers3_icon = Image.open("img/Pers5.png")
+        self.pers3_canv.icon = pers3_icon
+        pers3_icon = pers3_icon.resize((222, 222), Image.ANTIALIAS)
+        pers3_icon = ImageTk.PhotoImage(pers3_icon)
+        self.pers3 = self.pers3_canv.create_image(0, 15, image=pers3_icon, anchor=NW)
+        self.pers3_canv.image = pers3_icon
+        self.pers3_canv.bind("<Button-1>", partial(self.click_color_config, self.pers3_canv, "orange"))
+        self.pers3_canv.tag_bind(self.pers3, "<Button-3>", self.properties_pers3)
+
+        pers4_icon = Image.open("img/Pers8.png")
+        self.pers4_canv.icon = pers4_icon
+        pers4_icon = pers4_icon.resize((222, 222), Image.ANTIALIAS)
+        pers4_icon = ImageTk.PhotoImage(pers4_icon)
+        self.pers4 = self.pers4_canv.create_image(0, 15, image=pers4_icon, anchor=NW)
+        self.pers4_canv.image = pers4_icon
+        self.pers4_canv.bind('<Button-1>', partial(self.click_color_config, self.pers4_canv, "lightgreen"))
+        self.canvas.tag_bind(self.menu, '<Button-1>', self.close_choice_pers)
+        self.pers4_canv.tag_bind(self.pers4, "<Button-3>", self.properties_pers4)
+
+    def click_color_config(self, widget, color, event):
+        self.pers1_canv.config(bg="red")
+        self.pers2_canv.config(bg="blue")
+        self.pers3_canv.config(bg="yellow")
+        self.pers4_canv.config(bg="green")
+        widget.config(bg=color)
+        self.selected_pers = widget.icon
+
+    def close_choice_pers(self, event):
+        if self.menu:
+            self.canvas.delete(self.menu)
+        if self.pers_choice_label:
+            self.pers_choice_label.destroy()
+        self.menu_icon = Image.open("img/menu_button.png")
+        self.menu = self.menu_icon.resize((50, 50), Image.ANTIALIAS)
+        self.menu = ImageTk.PhotoImage(self.menu)
+        self.menu_canv = self.canvas.create_image(0, 0, image=self.menu, anchor=NW)
+        self.canvas.tag_bind(self.menu_canv, '<Button-1>', self.pers_choice)
+        if not self.selected_pers:
+            self.selected_pers = self.pers1_canv.icon
+        self.selected_pers = self.selected_pers.resize((350, 350), Image.ANTIALIAS)
+        self.selected_pers = ImageTk.PhotoImage(self.selected_pers)
+        self.canvas.create_image(250, 150, image=self.selected_pers)
+
+    def properties_pers1(self, event):
+        self.pers1_canv.delete(self.pers1)
+        self.pers1_canv.config(bg="red")
+        self.rect1 = self.pers1_canv.create_rectangle(10, 10, 214, 239, fill="yellow")
+        self.power1 = self.pers1_canv.create_text(60, 40, text="power:", font="Verdana 14")
+        self.power_value1 = self.pers1_canv.create_text(150, 40, text="16", font="Verdana 15 bold")
+        self.speed1 = self.pers1_canv.create_text(60, 80, text="speed:", font="Verdana 14")
+        self.speed_value1 = self.pers1_canv.create_text(150, 80, text="20", font="Verdana 15 bold")
+        self.health1 = self.pers1_canv.create_text(60, 120, text="health", font="Verdana 14")
+        self.health_value1 = self.pers1_canv.create_text(150, 120, text="100", font="Verdana 15 bold")
+        self.pers1_canv.tag_bind(self.rect1, '<Button-3>', self.back_pers1)
+        self.pers1_canv.tag_bind(self.power1, '<Button-3>', self.back_pers1)
+        self.pers1_canv.tag_bind(self.power_value1, '<Button-3>', self.back_pers1)
+        self.pers1_canv.tag_bind(self.speed1, '<Button-3>', self.back_pers1)
+        self.pers1_canv.tag_bind(self.speed_value1, '<Button-3>', self.back_pers1)
+        self.pers1_canv.tag_bind(self.health1, '<Button-3>', self.back_pers1)
+        self.pers1_canv.tag_bind(self.health_value1, '<Button-3>', self.back_pers1)
+
+    def properties_pers2(self, event):
+        self.pers2_canv.delete(self.pers2)
+        self.pers2_canv.config(bg="red")
+        self.rect2 = self.pers2_canv.create_rectangle(10, 10, 214, 239, fill="yellow")
+        self.power2 = self.pers2_canv.create_text(60, 40, text="power:", font="Verdana 14")
+        self.power_value2 = self.pers2_canv.create_text(150, 40, text="20", font="Verdana 15 bold")
+        self.speed2 = self.pers2_canv.create_text(60, 80, text="speed:", font="Verdana 14")
+        self.speed_value2 = self.pers2_canv.create_text(150, 80, text="20", font="Verdana 15 bold")
+        self.health2 = self.pers2_canv.create_text(60, 120, text="health", font="Verdana 14")
+        self.health_value2 = self.pers2_canv.create_text(150, 120, text="80", font="Verdana 15 bold")
+        self.pers2_canv.tag_bind(self.rect2, '<Button-3>', self.back_pers2)
+        self.pers2_canv.tag_bind(self.power2, '<Button-3>', self.back_pers2)
+        self.pers2_canv.tag_bind(self.power_value2, '<Button-3>', self.back_pers2)
+        self.pers2_canv.tag_bind(self.speed2, '<Button-3>', self.back_pers2)
+        self.pers2_canv.tag_bind(self.speed_value2, '<Button-3>', self.back_pers2)
+        self.pers2_canv.tag_bind(self.health2, '<Button-3>', self.back_pers2)
+        self.pers2_canv.tag_bind(self.health_value2, '<Button-3>', self.back_pers2)
+
+    def properties_pers3(self, event):
+        self.pers3_canv.delete(self.pers3)
+        self.pers3_canv.config(bg="red")
+        self.rect3 = self.pers3_canv.create_rectangle(10, 10, 214, 239, fill="yellow")
+        self.power3 = self.pers3_canv.create_text(60, 40, text="power:", font="Verdana 14")
+        self.power_value3 = self.pers3_canv.create_text(150, 40, text="14", font="Verdana 15 bold")
+        self.speed3 = self.pers3_canv.create_text(60, 80, text="speed:", font="Verdana 14")
+        self.speed_value3 = self.pers3_canv.create_text(150, 80, text="25", font="Verdana 15 bold")
+        self.health3 = self.pers3_canv.create_text(60, 120, text="health", font="Verdana 14")
+        self.health_value3 = self.pers3_canv.create_text(150, 120, text="110", font="Verdana 15 bold")
+        self.pers3_canv.tag_bind(self.rect3, '<Button-3>', self.back_pers3)
+        self.pers3_canv.tag_bind(self.power3, '<Button-3>', self.back_pers3)
+        self.pers3_canv.tag_bind(self.power_value3, '<Button-3>', self.back_pers3)
+        self.pers3_canv.tag_bind(self.speed3, '<Button-3>', self.back_pers3)
+        self.pers3_canv.tag_bind(self.speed_value3, '<Button-3>', self.back_pers3)
+        self.pers3_canv.tag_bind(self.health3, '<Button-3>', self.back_pers3)
+        self.pers3_canv.tag_bind(self.health_value3, '<Button-3>', self.back_pers3)
+
+    def properties_pers4(self, event):
+        self.pers4_canv.delete(self.pers4)
+        self.pers4_canv.config(bg="red")
+        self.rect4 = self.pers4_canv.create_rectangle(10, 10, 214, 239, fill="yellow")
+        self.power4 = self.pers4_canv.create_text(60, 40, text="power:", font="Verdana 14")
+        self.power_value4 = self.pers4_canv.create_text(150, 40, text="20", font="Verdana 15 bold")
+        self.speed4 = self.pers4_canv.create_text(60, 80, text="speed:", font="Verdana 14")
+        self.speed_value4 = self.pers4_canv.create_text(150, 80, text="10", font="Verdana 15 bold")
+        self.health4 = self.pers4_canv.create_text(60, 120, text="health", font="Verdana 14")
+        self.health_value4 = self.pers4_canv.create_text(150, 120, text="120", font="Verdana 15 bold")
+        self.pers4_canv.tag_bind(self.rect4, '<Button-3>', self.back_pers4)
+        self.pers4_canv.tag_bind(self.power4, '<Button-3>', self.back_pers4)
+        self.pers4_canv.tag_bind(self.power_value4, '<Button-3>', self.back_pers4)
+        self.pers4_canv.tag_bind(self.speed4, '<Button-3>', self.back_pers4)
+        self.pers4_canv.tag_bind(self.speed_value4, '<Button-3>', self.back_pers4)
+        self.pers4_canv.tag_bind(self.health4, '<Button-3>', self.back_pers4)
+        self.pers4_canv.tag_bind(self.health_value4, '<Button-3>', self.back_pers4)
+
+    def back_pers1(self, event):
+        self.pers1_canv.delete(self.rect1)
+        self.pers1_canv.config(bg="red")
+        self.pers1_canv.delete(self.power1)
+        self.pers1_canv.delete(self.power_value1)
+        self.pers1_canv.delete(self.speed1)
+        self.pers1_canv.delete(self.speed_value1)
+        self.pers1_canv.delete(self.health1)
+        self.pers1_canv.delete(self.health_value1)
+        pers1_icon = Image.open("img/Pers1.png")
+        self.pers1_canv.icon = pers1_icon
+        pers1_icon = pers1_icon.resize((222, 222), Image.ANTIALIAS)
+        pers1_icon = ImageTk.PhotoImage(pers1_icon)
+        self.pers1 = self.pers1_canv.create_image(0, 15, image=pers1_icon, anchor=NW)
+        self.pers1_canv.image = pers1_icon
+        self.pers1_canv.bind("<Button-1>", partial(self.click_color_config, self.pers1_canv, "darkred"))
+        self.pers1_canv.tag_bind(self.pers1, "<Button-3>", self.properties_pers1)
+
+    def back_pers2(self, event):
+        self.pers2_canv.delete(self.rect2)
+        self.pers2_canv.config(bg="blue")
+        self.pers2_canv.delete(self.power2)
+        self.pers2_canv.delete(self.power_value2)
+        self.pers2_canv.delete(self.speed2)
+        self.pers2_canv.delete(self.speed_value2)
+        self.pers2_canv.delete(self.health2)
+        self.pers2_canv.delete(self.health_value2)
+        pers2_icon = Image.open("img/Pers4.png")
+        self.pers2_canv.icon = pers2_icon
+        pers2_icon = pers2_icon.resize((222, 222), Image.ANTIALIAS)
+        pers2_icon = ImageTk.PhotoImage(pers2_icon)
+        self.pers2 = self.pers2_canv.create_image(0, 15, image=pers2_icon, anchor=NW)
+        self.pers2_canv.image = pers2_icon
+        self.pers2_canv.bind("<Button-1>", partial(self.click_color_config, self.pers2_canv, "lightblue"))
+        self.pers2_canv.tag_bind(self.pers2, "<Button-3>", self.properties_pers2)
+
+    def back_pers3(self, event):
+        self.pers3_canv.delete(self.rect3)
+        self.pers3_canv.config(bg="yellow")
+        self.pers3_canv.delete(self.power3)
+        self.pers3_canv.delete(self.power_value3)
+        self.pers3_canv.delete(self.speed3)
+        self.pers3_canv.delete(self.speed_value3)
+        self.pers3_canv.delete(self.health3)
+        self.pers3_canv.delete(self.health_value3)
+        pers3_icon = Image.open("img/Pers5.png")
+        self.pers3_canv.icon = pers3_icon
+        pers3_icon = pers3_icon.resize((222, 222), Image.ANTIALIAS)
+        pers3_icon = ImageTk.PhotoImage(pers3_icon)
+        self.pers3 = self.pers3_canv.create_image(0, 15, image=pers3_icon, anchor=NW)
+        self.pers3_canv.image = pers3_icon
+        self.pers3_canv.bind("<Button-1>", partial(self.click_color_config, self.pers3_canv, "orange"))
+        self.pers3_canv.tag_bind(self.pers3, "<Button-3>", self.properties_pers3)
+
+    def back_pers4(self, event):
+        self.pers4_canv.delete(self.rect4)
+        self.pers4_canv.config(bg="green")
+        self.pers4_canv.delete(self.power4)
+        self.pers4_canv.delete(self.power_value4)
+        self.pers4_canv.delete(self.speed4)
+        self.pers4_canv.delete(self.speed_value4)
+        self.pers4_canv.delete(self.health4)
+        self.pers4_canv.delete(self.health_value4)
+        pers4_icon = Image.open("img/Pers8.png")
+        self.pers4_canv.icon = pers4_icon
+        pers4_icon = pers4_icon.resize((222, 222), Image.ANTIALIAS)
+        pers4_icon = ImageTk.PhotoImage(pers4_icon)
+        self.pers4 = self.pers4_canv.create_image(0, 15, image=pers4_icon, anchor=NW)
+        self.pers4_canv.image = pers4_icon
+        self.pers4_canv.bind("<Button-1>", partial(self.click_color_config, self.pers4_canv, "lightgreen"))
+        self.pers4_canv.tag_bind(self.pers4, "<Button-3>", self.properties_pers4)
+
+Start_window()
