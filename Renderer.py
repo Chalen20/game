33fd7a3774c7a90a94a01e23b5f4779338fe89e7
Andrii Chaliuk <0632135592an@gmail.com>
2020-06-16 11:51:20 +0300

    commit

--- Renderer.py
+++ Renderer.py
@@ -1,29 +1,26 @@
 class Renderer:
-    def __init__(self,gui):
-        self.size=gui.size
-        self.gui=gui
-        self.x=self.gui.x
-        self.y=self.gui.y
-        self.canvas=self.gui.canvas
-    def renderChunk(self,chunk):
+    def __init__(self, gui):
+        self.size = gui.size
+        self.gui = gui
+        self.x = self.gui.x
+        self.y = self.gui.y
+        self.canvas = self.gui.canvas
+    def renderChunk(self, chunk):
        for i in chunk.tiles:
            for j in i:
-               self.renderTile(j,chunk.x,chunk.y,chunk)
+               self.renderTile(j, chunk.x, chunk.y, chunk)
     def renderTile(self,tile,cx,cy,chunk):
         
         size = self.size
         x= self.x+size*(tile.x+cx*chunk.size)
         y= self.y+size*(tile.y+cy*chunk.size)
         if not tile.connections[0]:
-            self.addWall(x,y,x+size,y+1)
+            self.addWall(x, y, x+size, y+1)
         if not tile.connections[1]:
-            self.addWall(x,y,x+1,y+size)
+            self.addWall(x, y, x+1, y+size)
         if not tile.connections[2]:
-            self.addWall(x+size,y+1,x+size,y+size)
+            self.addWall(x+size, y+1, x+size, y+size)
         if not tile.connections[3]:
-            self.addWall(x+1,y+size,x+size,y+size)
-    def addWall(self,x1,y1,x2,y2):
-        self.canvas.create_rectangle(x1,y1,x2,y2)
-        
-        
-    
+            self.addWall(x+1, y+size, x+size, y+size)
+    def addWall(self, x1, y1, x2, y2):
+        self.canvas.create_rectangle(x1, y1, x2, y2)
--- gameController.py
+++ gameController.py
@@ -1,92 +1,88 @@
 from Labyrynth import Maze
-#import pers
+#from pers import Pers
 from Renderer import Renderer
 from tkinter import*
-from time import*
+from time import *
 from testPers import*
 options={
-    'intensity':0.1,
-    'lifespan':8,
-    'loopchance':0.5,
-    'cavechance':0.5,
-    'chunk_size':18,
-    'block_chance':0.9,
-    'double_entrance':0.2
+    'intensity': 0.1,
+    'lifespan': 8,
+    'loopchance': 0.5,
+    'cavechance': 0.5,
+    'chunk_size': 18,
+    'block_chance': 0.9,
+    'double_entrance': 0.2
 }
 class GUI:
     
     def __init__(self):
-        self.size=75
-        self.x=5000
-        self.y=5000
-        root=Tk()
+        self.size = 75
+        self.x = 5000
+        self.y = 5000
+        root = Tk()
         self.canvas = Canvas(root, width=800, height=800, background="bisque")
-        ren=Renderer(self)
-        
-       
-        
-        
-        self.canvas.configure(scrollregion=(0,0,10000,10000))
+        ren = Renderer(self)
+
+        self.canvas.configure(scrollregion=(0, 0, 10000, 10000))
         self.canvas.pack()
         self.canvas.bind("<ButtonPress-1>", self.scroll_start)
         self.canvas.bind("<B1-Motion>", self.scroll_move)
 
-
-        options={
-            'intensity':0.1,
-            'lifespan':8,
-            'loopchance':0.5,
-            'cavechance':0.5,
-            'chunk_size':18,
-            'block_chance':0.9,
-            'double_entrance':0.2
+        options = {
+            'intensity': 0.1,
+            'lifespan': 8,
+            'loopchance': 0.5,
+            'cavechance': 0.5,
+            'chunk_size': 18,
+            'block_chance': 0.9,
+            'double_entrance': 0.2
         }
         self.maze=Maze(options)
-        self.maze.addChunk(0,0,0)
-        self.maze.addChunk(0,-1,0)
-        self.maze.addChunk(1,-1,0)
-        self.maze.addChunk(-1,0,0)
-        self.maze.addChunk(-1,1,0)
-        self.maze.addChunk(1,1,0)
-        self.maze.addChunk(1,0,0)
-        self.maze.addChunk(0,1,0)
-        self.maze.addChunk(-1,-1,0)
+        self.maze.addChunk(0, 0, 0)
+        self.maze.addChunk(0, -1, 0)
+        self.maze.addChunk(1, -1, 0)
+        self.maze.addChunk(-1, 0, 0)
+        self.maze.addChunk(-1, 1, 0)
+        self.maze.addChunk(1, 1, 0)
+        self.maze.addChunk(1, 0, 0)
+        self.maze.addChunk(0, 1, 0)
+        self.maze.addChunk(-1, -1, 0)
         
-        self.canvas.create_rectangle(0,0,20,20, outline="black", fill='black')
+        self.canvas.create_rectangle(0, 0, 20, 20, outline="black", fill='black')
         for i in self.maze.chunks:
             for j in self.maze.chunks[i]:
                 for t in self.maze.chunks[i][j]:
-                    if t==0: 
+                    if t == 0:
                         ren.renderChunk(self.maze.chunks[i][j][t])
-        self.canvas.scan_mark(0,0)
-        self.canvas.scan_dragto(-4570,-4570,gain=1)
+        self.canvas.scan_mark(0, 0)
+        self.canvas.scan_dragto(-4570, -4570, gain=1)
 
-        self.pers=TestPers(5000,5000,self.maze.get(0,0,0).tiles[0][0])
-        rect=self.canvas.create_rectangle(self.pers.x,self.pers.y,self.pers.x+10,self.pers.y+10,fill='red')
+        self.pers=TestPers(5000, 5000, self.maze.get(0, 0, 0).tiles[0][0])
+        rect=self.canvas.create_rectangle(self.pers.x, self.pers.y, self.pers.x+10, self.pers.y+10, fill='red')
         def onKeyUp(event):
-            print(1);
-            self.canvas.scan_mark(0,0)
-            self.canvas.scan_dragto(0,1)
-            self.canvas.move(rect,0,-self.pers.speed)
+            print(1)
+            self.canvas.scan_mark(0, 0)
+            self.canvas.scan_dragto(0, 1)
+            self.canvas.move(rect, 0, -self.pers.speed)
 
         def onKeyDown(event):
-            self.canvas.scan_mark(0,0)
-            self.canvas.scan_dragto(0,-1)
-            self.canvas.move(rect,0,self.pers.speed)
+            self.canvas.scan_mark(0, 0)
+            self.canvas.scan_dragto(0, -1)
+            self.canvas.move(rect, 0, self.pers.speed)
         
         def onKeyRight(event):
-            self.canvas.scan_mark(0,0)
-            self.canvas.scan_dragto(-1,0)
-            self.canvas.move(rect,self.pers.speed,0)
+            self.canvas.scan_mark(0, 0)
+            self.canvas.scan_dragto(-1, 0)
+            self.canvas.move(rect, self.pers.speed, 0)
         
         def onKeyLeft(event):
-            self.canvas.scan_mark(0,0)
-            self.canvas.scan_dragto(1,0)
-            self.canvas.move(rect,-self.pers.speed,0)
-        root.bind('<Left>',onKeyLeft)
-        root.bind('<Right>',onKeyRight)
-        root.bind('<Up>',onKeyUp)
-        root.bind('<Down>',onKeyDown)
+            self.canvas.scan_mark(0, 0)
+            self.canvas.scan_dragto(1, 0)
+            self.canvas.move(rect, -self.pers.speed, 0)
+        root.bind('<Left>', onKeyLeft)
+        root.bind('<Right>', onKeyRight)
+        root.bind('<Up>', onKeyUp)
+        root.bind('<Down>', onKeyDown)
         root.mainloop()
     def scroll_start(self, event):
         #print("from",event.x,event.y)
@@ -95,22 +91,22 @@ class GUI:
         #if(event.x>
         #print("to",event.x,event.y)
         self.canvas.scan_dragto(event.x, event.y, gain=1)
+
 class GameController:
     def __init__(self):
-        self.gui=GUI()
-        self.time=0
-        self.maze=Maze(options)
-        self.hero=Pers('pers1')
-        self.monsters={
-            'normal':[],
-            'minotaurs':[]
-
+        self.gui = GUI()
+        self.time = 0
+        self.maze = Maze(options)
+        #self.hero=Pers('pers1')
+        self.monsters = {
+            'normal': [],
+            'minotaurs': []
         }
-        self.renderer=Renderer()
-        self.villages=[]
+        self.renderer = Renderer()
+        self.villages = []
         
     def loop(self):
-        time+=0.05
+        self.time += 0.05
         
 GUI()
 
