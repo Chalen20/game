448420eccc49bfc3f88afec73101f24d68cc0bdd
Andrii Chaliuk <0632135592an@gmail.com>
2020-06-22 17:02:25 +0300

    drop

--- item.py
+++ item.py
@@ -1,52 +1,49 @@
 from random import *
+from PIL import Image, ImageTk
+
+class Item:
+    def __init__(self, skin, bigSkin, name):
+        self.skin = skin
+        self.bigSkin = bigSkin
+        self.name = name
+
+class Food(Item):
+    def __init__(self, skin, bigSkin, name, satiety, *args):
+        Item(skin, bigSkin, name)
+        self.rottness = 100
+        self.satiety = satiety
+        if (len(args) > 0):
+            self.canRott = True
+            self.rotteRate = args[0]
+            # self.rottness=100
+        else:
+            self.canRott = False
+
+    def rotten(self):
+        if (self.canRott):
+            self.rottness -= self.rotteRate
+        if (self.rottness <= 0):
+            return True
+        return False
+
 class ItemController:
-    possibleItem=[Food(getImage("img/food/Food_varenics.png",False))]
-    def getLoot(self):
-        item=possibleItem[randint(0,len(possibleItem)-1)]
-        return item
-    def getImage(self,name,big):
+    def __init__(self):
+        self.possibleItems = [
+            Food(self.getImage("img/food/Food_apple.png", False), self.getImage("img/food/Food_apple.png", True),
+                 "apple", 10, 0.05)]
+
+    def getImage(self, name, big):
         img = Image.open(name)
         if big:
             img = img.resize((200, 200), Image.ANTIALIAS)
-            
+
         else:
             img = img.resize((100, 100), Image.ANTIALIAS)
-        return ImageTk.PhotoImage(img)   
-       
-        
+        return ImageTk.PhotoImage(img)
 
-class Item:
-    def __init__(self,skin,value,bigSkin,name)
-        self.skin=skin
-        self.value=value
-        self.bigSkin=name
-        self.name=name
-    
+    def drop(self, canvas, pers_x, pers_y):
+        canvas.create_image(pers_x, pers_y, image=self.possibleItems[0])
 
-class Food(Item):
-    def __init__(self,skin,value,bigSkin,name,satiety,*args):
-        Item(self,skin,value,bigSkin,name)
-        self.rottness=100
-        self.satiety=satiety
-        if(len(args)>0):
-            self.canRott=True
-            self.rotteRate=args[0]
-            #self.rottness=100
-        else:
-            self.canRott=False
-            
-    def rotten(self):
-        if(self.canRott):
-            self.rottness-=self.rotteRate
-        if(self.rottness<=0):
-            return True
-        return False
-    def use(self,pers):
-        pers.hunger-=self.satiety
-        if(pers.hunger<0):
-            pers.hunger=0
-        return True
 #class Armor(Item):
-class Weapon(Item):
-class Poitions(Item):
-    
+#class Weapon(Item):
+#class Poitions(Item):
