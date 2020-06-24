e74df698274064d736a9e462aca4da1b2adc6892
ili2908 <illia.kovalenko@ukma.edu.ua>
2020-06-24 13:43:43 +0300

    'm'

--- monsters.py
+++ monsters.py
@@ -2,10 +2,15 @@ from random import *
 from math import*
 from PIL import Image, ImageTk
 from backpack import Backpack
+from copy import copy, deepcopy
 class Monster():
 
-    def __init__(self, name,tile,gui):
+    def __init__(self, name,tile,gui,*args):
         self.gui=gui
+        if(len(args)>0):
+            self.lvl=args[0]
+        else:
+            self.lvl=0
         front_skin1 = Image.open("img/Orc2.png")
         front_skin1 = front_skin1.resize((100, 100), Image.ANTIALIAS)
         front_skin2 = Image.open("img/Orc3.png")
@@ -25,22 +30,35 @@ class Monster():
         front_skin4 = Image.open("img/portal.png")
         front_skin4 = front_skin4.resize((100, 100), Image.ANTIALIAS)
         
+        front_skin5 = Image.open("img/Orc1.png")
+        front_skin5 = front_skin5.resize((100, 100), Image.ANTIALIAS)
 
+        front_skin6 = Image.open("img/Orc5.png")
+        front_skin6 = front_skin6.resize((100, 100), Image.ANTIALIAS)
+
+        front_skin7 = Image.open("img/portal.png")
+        front_skin7 = front_skin7.resize((20, 20), Image.ANTIALIAS)
+        
         front_skin3 = Image.open("img/full_chest.png")
         front_skin3 = front_skin3.resize((100, 100), Image.ANTIALIAS)
 
-        allMonsters = {"deathMonster": [ImageTk.PhotoImage(front_skin1), "dead_skin", "bot_skin", 15, 0.8, "arr_attack",
+        allMonsters = {"deathMonster": [ImageTk.PhotoImage(front_skin1), "dead_skin", 30, 30, 0.8, 10,
                                         ImageTk.PhotoImage(front_skin1_animation),
-                                        ImageTk.PhotoImage(front_skin1_animation2)],
-                       'deadlyMonster':[ImageTk.PhotoImage(front_skin2), "dead_skin", "bot_skin", 5, 1.6, "arr_attack",
+                                        ImageTk.PhotoImage(front_skin1_animation2), False],
+                       'deadlyMonster':[ImageTk.PhotoImage(front_skin2), "dead_skin", 30, 5, 1.6, 5,
                                         ImageTk.PhotoImage(front_skin2_animation),
-                                        ImageTk.PhotoImage(front_skin2_animation2)],
-                       'chest':[ImageTk.PhotoImage(front_skin3), "dead_skin", "bot_skin", 1, 0, "arr_attack",
-                                ImageTk.PhotoImage(front_skin3), ImageTk.PhotoImage(front_skin3)],
-                       'portal':[ImageTk.PhotoImage(front_skin4), "dead_skin", "bot_skin", 1, 0, "arr_attack",
-                                 ImageTk.PhotoImage(front_skin4), ImageTk.PhotoImage(front_skin4)]
-                       
-
+                                        ImageTk.PhotoImage(front_skin2_animation2), False],
+                       'chest':[ImageTk.PhotoImage(front_skin3), "dead_skin", 0, 1, 0, 0,
+                                ImageTk.PhotoImage(front_skin3), ImageTk.PhotoImage(front_skin3), False],
+                       'portal':[ImageTk.PhotoImage(front_skin4), "dead_skin", 0, 100000, 0, 0,
+                                 ImageTk.PhotoImage(front_skin4), ImageTk.PhotoImage(front_skin4), False],
+                       'death':[ImageTk.PhotoImage(front_skin5), "dead_skin", 20, 1, 0.6, 50,
+                                 ImageTk.PhotoImage(front_skin5), ImageTk.PhotoImage(front_skin5), False],
+                       'mage':[ImageTk.PhotoImage(front_skin6), "dead_skin", 200, 1, 0.6, 0,
+                                 ImageTk.PhotoImage(front_skin6), ImageTk.PhotoImage(front_skin6), False],
+                       'fireball':[ImageTk.PhotoImage(front_skin7), "dead_skin", 5, 1, 2, 5,
+                                 ImageTk.PhotoImage(front_skin7), ImageTk.PhotoImage(front_skin7), True]
+                        
                        }
         self.tile = tile
         self.skin = allMonsters[name][0]
@@ -50,13 +68,13 @@ class Monster():
         self.died_skin = allMonsters[name][1]
         self.faced_north = True
         self.faced_east = False
-        self.bot_skin = allMonsters[name][2]
+        self.attackRange = allMonsters[name][2]
         self.x = tile.realx+50+random()*13
         self.y = tile.realy+50+random()*13
-
+        self.missile=allMonsters[name][8]
         self.skin_animation = allMonsters[name][6]
         self.skin_animation2 = allMonsters[name][7]
-
+        self.power=allMonsters[name][5]
         self.isDied = False
         self.target = None
         self.image = False
@@ -66,9 +84,19 @@ class Monster():
         #self.attack=5
         self.q =False
         
-    def attack(self):
-        #attack_value = randint(0.7 * allMonsters[self.name][5], 1.3 * allMonsters[self.name][5])
-        return 5
+    def attack(self,pers,mcb):
+        class Point:
+            def __init__(self,x,y):
+                self.x=x
+                self.y=y
+        if self.name == 'mage':
+            mcb.monsters.append(Monster('fireball',self.tile,self.gui,self.lvl))
+                    #print(con)
+            mcb.monsters[-1].target=Point(copy(pers.x),copy(pers.y))
+            mcb.monsters[-1].x=self.x
+            mcb.monsters[-1].y=self.y
+        attack_value = self.power + randint(0,self.lvl)*(1+self.lvl*0.7)
+        return attack_value
 
     def die(self):
         meat = Image.open("img/food/meat.png")
@@ -83,7 +111,13 @@ class Monster():
         if self.name=='chest':
             self.gui.items.append([meat, meat_big, "meat", "food", 20, 0.04, True])
 
-    def move_toward(self,x,y):
+    def move_toward(self,x,y,*args):
+        if(sqrt((x-self.x)**2+(y-self.y)**2)<self.attackRange-3):
+            if(self.missile):
+                self.die()
+            else:
+                if(not len(args)>0):
+                    return
         gui=self.gui
         velx=x - self.x
         vely=y - self.y
@@ -116,7 +150,7 @@ class Monster():
 
                     self.tile = self.tile.connections[1]
                     #print(self.tile.x,self.tile.y)
-
+            
 
 
     def move_top(self):
@@ -171,21 +205,30 @@ class MonsterCollectiveBrain:
         self.pers=gui.pers
         self.maze=gui.maze
         self.monsterCount=0
-        self.monsters.append(Monster("deathMonster",self.maze.getTile(3,3,0),gui))
+        #self.monsters.append(Monster("deathMonster",self.maze.getTile(3,3,0),gui))
         self.monsters[-1].target=self.monsters[-1].tile
         #print(self.monsters[-1].target.x,self.monsters[-1].target.y)
     def loop(self,gui):
-        possible = ['deathMonster','deadlyMonster','deathMonster']
+        lvl=self.pers.tile.chunk.z
+        possible = {0:['deathMonster','deadlyMonster','mage'],
+                    1:['deathMonster','deadlyMonster','death','deathMonster'],
+                    2:['deathMonster','deadlyMonster','deathMonster'],
+                    3:['deathMonster','deadlyMonster','deathMonster'],
+                    4:['deathMonster','deadlyMonster','deathMonster'],
+                    5:['deathMonster','deadlyMonster','deathMonster']
+                    }
+        possible=possible[lvl]
         #print(self.monsterCount)
-        if(not self.monsterCount>3):
+        if(not self.monsterCount>3+lvl):
             #print(self.monsterCount)
             #self.monsterCount+=1
             if len(gui.visible[1])-1>0:
+                #print(self.monsterCount)
                 tile = gui.visible[1][randint(0, floor(len(gui.visible[1])-1))]
                 con=tile.connections[randint(0,3)]
                 if con and not con in gui.visible[0]:
                     self.monsterCount+=1
-                    self.monsters.append(Monster(possible[randint(0,len(possible)-1)],con,gui))
+                    self.monsters.append(Monster(possible[randint(0,len(possible)-1)],con,gui,lvl))
                     #print(con)
                     self.monsters[-1].target=tile
                     pass
@@ -196,7 +239,7 @@ class MonsterCollectiveBrain:
                 if(random()<0.4 and self.pers.tile.room.isFree):
                     #print(2)
                     #self.monsterCount+=1
-                    self.monsters.append(Monster(possible[randint(0,len(possible)-1)],i,gui))
+                    self.monsters.append(Monster(possible[randint(0,len(possible)-1)],i,gui,lvl))
                     self.monsters[-1].target=i
                     if(random()<0.5):
                         self.monsters.append(Monster('chest',i,gui))
@@ -209,32 +252,50 @@ class MonsterCollectiveBrain:
                     self.monsters[-1].q=True
             self.pers.tile.room.isFree=False
         for i in self.monsters:
+            if i.lvl != lvl:
+                continue
             if i.lifespan>2:
                 i.die()
             if i.isDied:
+                #print(i.missile)
                 self.monsters.remove(i)
-                if(not i.q):
+                if(not i.q and not i.missile):
                     self.monsterCount-=1
+                if(i.missile):
+                    if(sqrt((i.x-self.pers.x)**2+(i.y-self.pers.y)**2)<i.attackRange+5): 
+                        attack= i.attack(self.pers,self)     
+                        self.pers.take_damage(attack)
+                        
+                        now_health = gui.health.point - attack
+                        gui.health.change(now_health)
             elif i.tile in gui.visible[0]:
                 #print('1')
-                i.move_toward(self.pers.x, self.pers.y)
+                if(i.missile):
+                    i.move_toward(i.target.x,i.target.y)
+                else:
+                    i.move_toward(self.pers.x, self.pers.y)
                 i.redraw(gui.canvas)
                 i.counter += 1
             else:
-                target=i.target
-                i.move_toward(target.realx+50,target.realy+50)
+                if(i.missile):
+                    i.move_toward(i.target.x,i.target.y)
+                else:
+                    target=i.target
+                    i.move_toward(target.realx+50,target.realy+50,True)
+                #target=i.target
+                #i.move_toward(target.realx+50,target.realy+50)
                 if(not i.q):
                     i.lifespan+=0.01
-            if(sqrt((i.x-self.pers.x)**2+(i.y-self.pers.y)**2)<30 and i.recharge<=0):
+            if(sqrt((i.x-self.pers.x)**2+(i.y-self.pers.y)**2)<i.attackRange and i.recharge<=0):
                 if(i.name=='portal'):
                     if i.tile.chunk.z==0:
                         gui.level(1)
                     if i.tile.chunk.z==1:
                         gui.level(0)
-                       
-                self.pers.take_damage(i.attack())
+                attack= i.attack(self.pers,self)     
+                self.pers.take_damage(attack)
                 i.recharge=2
-                now_health = gui.health.point - i.attack()
+                now_health = gui.health.point - attack
                 gui.health.change(now_health)
             if(i.recharge!=0):
                 i.recharge-=0.01
